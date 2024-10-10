3a45b292985a29918c8bd294fa48a23245369696
Jah Seng Lee <jahseng.lee@gmail.com>
2024-10-10 21:47:39 +1300

    Add jest specs for Navbar

---
+++ app/javascript/bundles/Navbar.spec.jsx
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import Navbar from "./App/components/Navbar";
+
+const mockOnClick = jest.fn();
+
+test('clicking links calls the onClick function', async () => {
+  render(<Navbar onClick={mockOnClick} />);
+
+  await userEvent.click(screen.getByText("IBM"));
+  expect(mockOnClick).toHaveBeenCalledWith("IBM");
+
+  await userEvent.click(screen.getByText("Tesla"));
+  expect(mockOnClick).toHaveBeenCalledWith("TSLA");
+
+  await userEvent.click(screen.getByText("Gamestop"));
+  expect(mockOnClick).toHaveBeenCalledWith("GME");
+});
